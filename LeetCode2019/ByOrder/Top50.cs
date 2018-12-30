151b3cff84673e42fdd1a469b301ba771ea22720
han wang <hwang@hans-MacBook-Pro.local>
2018-12-29 17:02:53 -0800

    12/29/2018

--- LeetCode2019/ByOrder/Top50.cs
+++ LeetCode2019/ByOrder/Top50.cs
@@ -52,6 +52,97 @@ namespace LeetCode2019.ByOrder
 
         //========================================================================//
         //----------2. Add Two Numbers--------------------------------------------//
+        /*You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+            You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
+        Example:
+
+        Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+        Output: 7 -> 0 -> 8
+        Explanation: 342 + 465 = 807.*/
+        
+        //-----------------Notes--------------------------------------------------//
+        /* Time: O(n) Space: O(n) say n is longer length of L1 and l2
+           这道题主要注意优化代码， 为了避免冗余代码需要check如果node为null的情况。
+           运算结束需要考虑carry 是不是1 如果是则还需一位n*/
+        public ListNode AddTwoNumber(ListNode l1, ListNode l2)
+        {
+            ListNode node1 = l1;
+            ListNode node2 = l2;
+            ListNode dummy = new ListNode(0);
+            ListNode res = dummy;
+            int carry = 0;
+            while(node1!=null || node2!=null)
+            {
+                int a = (node1==null)?0:node1.val;
+                int b = (node2==null)?0:node2.val;
+                res.next = new ListNode((a+b+carry)%10);
+                carry = (a+b+carry)/10;
+                node1 = (node1==null)?null: node1.next;
+                node2 = (node2==null)?null: node2.next;
+                res = res.next;
+            }
+            if(carry==1)
+            {
+                res.next = new ListNode(1);
+            }
+            return dummy.next;
+        }
+        //========================================================================//
+        //----------3. 3. Longest Substring Without Repeating Characters----------//
+        /*Given a string, find the length of the longest substring without repeating characters.
+
+            Example 1:
+
+            Input: "abcabcbb"
+            Output: 3 
+            Explanation: The answer is "abc", with the length of 3. 
+            Example 2:
+
+            Input: "bbbbb"
+            Output: 1
+            Explanation: The answer is "b", with the length of 1.
+            Example 3:
+
+            Input: "pwwkew"
+            Output: 3
+            Explanation: The answer is "wke", with the length of 3. 
+        Note that the answer must be a substring, "pwke" is a subsequence and not a substring.*/
+        
+        //-----------------Notes--------------------------------------------------//
+        /* Time: O(n) Space: O(n) 
+           Sliding window, and track visited characters in hash map.
+           Remember the start position and end position, 
+           if the char in end position exists in hashmap and the index is greater or equal to the start position which means there are duplicates.
+           so slid the window to change the start position to the right of the first appreance of the duplicate char*/
+        public int LengthOfLongestSubstring(string s) 
+        {
+            if(string.IsNullOrEmpty(s))
+            {
+                return 0;
+            }
+            Dictionary<char, int> map = new Dictionary<char, int>();
+            int start = 0, end = 0;
+            int res = 0;
+            while(end<s.Length)
+            {
+                if(map.ContainsKey(s[end]))
+                {
+                    if(map[s[end]]>=start)
+                    {
+                        start = map[s[end]]+1;
+                    }
+                    map[s[end]]=end;
+                }
+                else
+                {
+                    map.Add(s[end], end);
+                }
+
+                res= Math.Max(res, end-start+1);
+                end++;
+            }
+            return res;
+        }
     }
 }
