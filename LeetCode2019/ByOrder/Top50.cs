9484b4a49ef6b92872b1932a4747030dfcaa16aa
han wang <hwang@hans-MacBook-Pro.local>
2019-01-10 21:09:34 -0800

    01/10/2019

--- LeetCode2019/ByOrder/Top50.cs
+++ LeetCode2019/ByOrder/Top50.cs
@@ -12,6 +12,7 @@ namespace LeetCode2019.ByOrder
     4. Median of Two Sorted Arrays--Hard
     5. Longest Palindromic Substring--Medium
     10. Regular Expression Matching--Hard
+    15. 3 Sum--Medium
      */
     public class Top50
     {
@@ -652,6 +653,147 @@ namespace LeetCode2019.ByOrder
             }
             return res;
         }
-    
+        //========================================================================================//
+        //----------14. Longest Common Prefix--Easy-----------------------------------------------//
+        /*
+        Write a function to find the longest common prefix string amongst an array of strings.
+
+        If there is no common prefix, return an empty string "".
+
+        Example 1:
+
+        Input: ["flower","flow","flight"]
+        Output: "fl"
+        Example 2:
+
+        Input: ["dog","racecar","car"]
+        Output: ""
+        Explanation: There is no common prefix among the input strings.
+        */
+        public string LongestCommonPrefix(string[] strs) 
+        {
+            if(strs==null||strs.Length==0)
+                return "";
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0;i<strs[0].Length; i++)
+            {
+                for(int j = 0; j<strs.Length; j++)
+                {
+                    if(i>=strs[j].Length  || strs[j][i]!=strs[0][i])
+                    {
+                        return sb.ToString();
+                    }
+                }
+                sb.Append(strs[0][i]);
+            }
+            return sb.ToString();
+        }
+
+        //========================================================================================//
+        //----------15. 3 Sum--Medium-------------------------------------------------------------//
+        /*
+        Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+
+        Note:
+
+        The solution set must not contain duplicate triplets.
+
+        Example:
+
+        Given array nums = [-1, 0, 1, 2, -1, -4],
+
+        A solution set is:
+        [
+        [-1, 0, 1],
+        [-1, -1, 2]
+        ]
+         */
+        
+        /*---------------------Notes----------------------------------------------------------- */
+        /*
+        Two pointer
+        Time complexity: O(n^2+nlogn) 则为 O（n^2）; space complexity O(1);
+        这个题算是two sum 变形，为了避免重复则需要排序以便跳过重复的数字。
+        过一遍数组 在当前index 之后的所有数里找所有可能的两个数与当前数之和为0的结果， 这一步则把问题变成了找two sum
+        需要注意的是当碰到当前的数 与前一个index的数相同时则需要跳过 以避免重复的结果。
+         */
+
+        public IList<IList<int>> ThreeSum(int[] nums) 
+        {
+            Array.Sort(nums);
+            IList<IList<int>> res = new List<IList<int>>();
+            for(int i =0; i<nums.Length; i++)
+            {
+                //if duplicated element, then skip
+                if(i>0 && nums[i]==nums[i-1])
+                    continue;
+                //Find all twosum results
+                int p1=i+1, p2 = nums.Length-1;
+                while(p1<p2)
+                {
+                    //skip duplicated element
+                    if(p1>i+1 && nums[p1]==nums[p1-1])
+                    {
+                        p1++;
+                        continue;
+                    }
+                    if(p2<nums.Length-1 && nums[p2]==nums[p2+1])
+                    {
+                        p2--;
+                        continue;
+                    }
+
+                    //if result, add to the res.
+                    //if greater, move p2
+                    //if smaller, move p1
+                    if(nums[p1]+nums[p2]==-1*nums[i])
+                    {
+                        res.Add(new List<int>{nums[i], nums[p1], nums[p2]});
+                        p1++; p2--;
+                    }
+                    else if(nums[p1]+nums[p2]>-1*nums[i])
+                    {
+                        p2--;
+                    }
+                    else
+                        p1++;
+                }
+            }
+            return res;
+        }
+        //========================================================================================//
+        //----------16. 3 Sum Closest--Medium-------------------------------------------------------------//
+        /*
+        Given an array nums of n integers and an integer target, find three integers in nums such that the sum is closest to target. Return the sum of the three integers. You may assume that each input would have exactly one solution.
+
+        Example:
+
+        Given array nums = [-1, 2, 1, -4], and target = 1.
+
+        The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+         */
+        public int ThreeSumClosest(int[] nums, int target)
+        {
+            int closest = nums[0]+nums[1]+nums[2];
+            Array.Sort(nums);
+            for(int i =0; i<nums.Length; i++)
+            {
+                int p1=i+1; int p2 = nums.Length-1;
+                while(p1<p2)
+                {
+                    int sum = nums[i]+nums[p1]+nums[p2];
+                    closest = Math.Abs(closest-target)>Math.Abs(sum-target)?sum:closest;
+                    if(nums[p1]+nums[p2]>target-nums[i])
+                    {
+                    p2--;
+                    }
+                    else
+                    {
+                        p1++;
+                    }
+                }
+            }
+            return closest;
+        }
     }
 }
